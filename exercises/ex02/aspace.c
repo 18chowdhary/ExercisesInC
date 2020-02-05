942daeb22c171ced6cfd8481f3bd49abf9029040
18chowdhary <schowdhary@olin.edu>
2020-02-05 16:02:04 -0500

    Completed HW02

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -10,17 +10,30 @@ License: GNU GPLv3
 
 int var1;
 
+void print_address() {
+  int local = 0;
+  printf("Address is %p\n", &local);
+}
+
 int main ()
 {
     int var2 = 5;
-    void *p = malloc(128);
-    char *s = "Hello, World";
+    void* p = malloc(128);
+    char* s = "Hello, World";
+    void* q = malloc(128);
+    print_address();
+
+    void* chunk1 = malloc(32);
+    void* chunk2 = malloc(32);
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
     printf ("s points to %p\n", s);
+    printf("q points to %p\n", q);
+    printf("Address of chunk1: %p\n", chunk1);
+    printf("Address of chunk2: %p\n", chunk2);
 
     return 0;
 }
